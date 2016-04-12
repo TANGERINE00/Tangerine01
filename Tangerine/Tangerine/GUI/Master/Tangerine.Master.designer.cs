0879e6cb4ec886609050a53ce9a9147a8f84cbd7
cesararod <carr235@gmail.com>
2016-04-12 18:25:34 -0430

    commit

--- Tangerine/Tangerine/GUI/Master/Tangerine.Master.designer.cs
+++ Tangerine/Tangerine/GUI/Master/Tangerine.Master.designer.cs
@@ -74,7 +74,7 @@ namespace Tangerine.GUI.Master {
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
         protected global::System.Web.UI.WebControls.ContentPlaceHolder ContentPlaceHolder1;
-
+        
         /// <summary>
         /// contenidoCentral control.
         /// </summary>
