b9d65bd3b0aa99c94e3dd05a89276293afc5099c
cesararod <carr235@gmail.com>
2016-04-26 10:26:42 -0430

    Basico de conexion y excepciones

---
+++ Tangerine/Tangerine/DatosTangerine/Resultado.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DatosTangerine
+{
+    /// <summary>
+    /// Clase para el manejo de los resultados de los query's de base de datos
+    /// </summary>
+    public class Resultado
+    {
+
+        public string etiqueta { get; set; }
+        public string valor { get; set; }
+
+        /// <summary>
+        /// Constructor de Resultado, no deberia existir resultado sin estos parametros
+        /// </summary>
+        /// <param name="etiqueta">Etiqueta del resultado ejemplo: @Nombre</param>
+        /// <param name="valor">Valor del resultado, ejemplo: Pepe</param>
+        public Resultado(string etiqueta, string valor)
+        {
+            this.etiqueta = etiqueta;
+            this.valor = valor;
+        }
+    }
+}
