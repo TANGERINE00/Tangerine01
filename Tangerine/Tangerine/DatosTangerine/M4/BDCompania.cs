09af444d5a02dc99918a0c6870666c909f774369
Javier Merchán <merchan1395@gmail.com>
2016-04-28 19:59:05 -0430

    Domino + Cascaron de BD
    -Dominio Compania
    -Cascaraon de archivo de BD Modulo 4

---
+++ Tangerine/Tangerine/DatosTangerine/M4/BDCompania.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data;
+using System.Data.Sql;
+using System.Data.SqlClient;
+using DominioTangerine;
+
+namespace DatosTangerine.M4
+{
+    public class BDCompania
+    {
+        /*
+        BDConexion theConnection;
+        List<Parametro> parameters;
+        Parametro theParam = new Parametro();
+        Boolean retBool = false;
+        
+        public Boolean AddContact(Contacto theContact)
+        {
+            parameters = new List<Parametro>();
+            theConnection = new BDConexion();
+
+            try
+            {
+
+                theParam = new Parametro(ResourceContact.ParamIdContact, SqlDbType.Int, theContact.IdContacto.ToString(), false);
+                parameters.Add(theParam);
+
+                List<Resultado> results = theConnection.EjecutarStoredProcedure(ResourceContact.AddNewContact, parameters);
+
+            }
+         
+            catch (SqlException ex)
+            {
+                //throw new ExcepcionesTangerine.OtrasExcep();
+            }
+
+            return retBool;
+        }
+     */
+
+    }
+}
---
+++ Tangerine/Tangerine/DominioTangerine/Compania.cs
@@ -0,0 +1,87 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioTangerine
+{
+    public class Compania
+    {
+        #region Atributos
+
+        private int idCompania;
+        private string nombreCompania;
+        private string rifCompania;
+        private string emailCompania;
+        private string acronimoCompania;
+        private DateTime fechaRegistroCompania;
+
+        #endregion
+
+        #region Get's Set's
+
+        public int IdCompania
+        {
+            get { return idCompania; }
+            set { idCompania = value; }
+        }
+
+        public string NombreCompania
+        {
+            get { return nombreCompania; }
+            set { nombreCompania = value; }
+        }
+
+        public string RifCompania
+        {
+            get { return rifCompania; }
+            set { rifCompania = value; }
+        }
+
+        public string EmailCompania
+        {
+            get { return emailCompania; }
+            set { emailCompania = value; }
+        }
+
+        public string AcronimoCompania
+        {
+            get { return acronimoCompania; }
+            set { acronimoCompania = value; }
+        }
+
+        public DateTime FechaRegistroCompania
+        {
+            get { return fechaRegistroCompania; }
+            set { fechaRegistroCompania = value; }
+        }
+
+        #endregion
+
+        #region Constructores
+
+        public Compania()
+        {
+            idCompania = 0;
+            nombreCompania = String.Empty;
+            rifCompania = String.Empty;
+            emailCompania = String.Empty;
+            acronimoCompania = String.Empty;
+            fechaRegistroCompania = DateTime.Now;
+        }
+
+        public Compania(Compania laCompania)
+        {
+            this.idCompania = laCompania.idCompania;
+            this.nombreCompania = laCompania.nombreCompania;
+            this.rifCompania = laCompania.rifCompania;
+            this.emailCompania = laCompania.emailCompania;
+            this.acronimoCompania = laCompania.acronimoCompania;
+            this.fechaRegistroCompania = laCompania.fechaRegistroCompania;
+        }
+
+        #endregion
+    }
+
+}
