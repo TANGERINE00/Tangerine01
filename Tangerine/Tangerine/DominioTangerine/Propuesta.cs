bf18df00aea550b16424585b2c28c18373ba649e
EddCold <ejpacheco.11@est.ucab.edu.ve>
2016-05-03 15:49:28 -0400

    CambiosVarios_M6
    cambios en la vista y creacion del dominio

---
+++ Tangerine/Tangerine/DominioTangerine/Propuesta.cs
@@ -0,0 +1,34 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioTangerine
+{
+    class Propuesta
+    {
+        #region Atributos
+
+        /// <summary>
+        /// Clase propuesta
+        /// <attr name="codigo">Codigo unico indentificador de la propuesta</attr>
+        /// <attr name="nombre">Nombre del proyecto</attr>
+        /// <attr name="estatus">Estado en el que se encuentra la propuesta {Aprobada, Pendiente, Cerrada}</attr>
+        /// <attr name="descripcion">Descripcion breve sobre la propuesta</attr>
+        /// <attr name="moneda">Moneda base de pago para la propuesta de proyecto</attr>
+        /// <attr name="costo">Costo de realizacion del 100% del proyecto</attr>
+        /// </summary>
+
+        private String codigo;
+        private String nombre;
+        private List<Compania> listaCompania;
+        private String descripcion;
+        private String moneda;
+        private int costo;
+
+        #endregion
+
+      
+    }
+}
