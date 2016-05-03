917446594b86c42e8b1556b380da2bfde7e66b7e
EddCold <ejpacheco.11@est.ucab.edu.ve>
2016-05-03 17:01:41 -0400

    Requerimiento_M6
    algunos cambios

--- Tangerine/Tangerine/DominioTangerine/Propuesta.cs
+++ Tangerine/Tangerine/DominioTangerine/Propuesta.cs
@@ -13,20 +13,31 @@ namespace DominioTangerine
         /// <summary>
         /// Clase propuesta
         /// <attr name="codigo">Codigo unico indentificador de la propuesta</attr>
-        /// <attr name="nombre">Nombre del proyecto</attr>
-        /// <attr name="estatus">Estado en el que se encuentra la propuesta {Aprobada, Pendiente, Cerrada}</attr>
+        /// <attr name="nombre">nombre con el que se identificara a la propuesta</attr>
         /// <attr name="descripcion">Descripcion breve sobre la propuesta</attr>
+        /// <attr name="duracion">duracion estimada de la propuesta {Meses, Dias, Horas}</attr>
+       
+        
         /// <attr name="moneda">Moneda base de pago para la propuesta de proyecto</attr>
         /// <attr name="costo">Costo de realizacion del 100% del proyecto</attr>
+        /// <attr name="estatus">Estado en el que se encuentra la propuesta {Aprobada, Pendiente, Cerrada}</attr>
         /// </summary>
 
-        private String codigo;
-        private String nombre;
-        private List<Compania> listaCompania;
-        private String descripcion;
-        private String moneda;
-        private int costo;
+        private String _codigo;
+        private String _nombre;
+        private String _descripcion;
+        private String _duracion;
+        private String _acuerdopago;
+        private String _estatus;
+        private int _entrega;
+        private DateTime _feincio;
+        private DateTime _fefinal;
+        private String _moneda;
+        private int _costo;
 
+        private List<Compania> _listaCompania;
+        private List<Requerimiento> _listaRequerimiento;
+      
         #endregion
 
       
