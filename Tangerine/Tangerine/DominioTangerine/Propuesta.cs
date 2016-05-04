0417de4d8a8dac036e629fca99f3231701c7ed34
EddCold <ejpacheco.11@est.ucab.edu.ve>
2016-05-04 18:22:49 -0400

    Arreglo_M6
    arreglo menor

--- Tangerine/Tangerine/DominioTangerine/Propuesta.cs
+++ Tangerine/Tangerine/DominioTangerine/Propuesta.cs
@@ -12,15 +12,19 @@ namespace DominioTangerine
 
         /// <summary>
         /// Clase propuesta
-        /// <attr name="codigo">Codigo unico indentificador de la propuesta</attr>
-        /// <attr name="nombre">nombre con el que se identificara a la propuesta</attr>
-        /// <attr name="descripcion">Descripcion breve sobre la propuesta</attr>
-        /// <attr name="duracion">duracion estimada de la propuesta {Meses, Dias, Horas}</attr>
-       
-        
-        /// <attr name="moneda">Moneda base de pago para la propuesta de proyecto</attr>
+        /// <attr name="_codigo">Codigo unico indentificador de la propuesta</attr>
+        /// <attr name="_nombre">nombre con el que se identificara a la propuesta</attr>
+        /// <attr name="_descripcion">Descripcion breve sobre la propuesta</attr>
+        /// <attr name="_duracion">duracion estimada de la propuesta {Meses, Dias, Horas}</attr>
+        /// <attr name="_acuerdopago">tipo de compromiso de pago al cual se va a llegar para la propuesta</attr>
+        /// <attr name="_estatus">Estado en el que se encuentra la propuesta {Aprobada, Pendiente, Cerrada}</attr>
+        /// <attr name="_moneda">Moneda base de pago para la propuesta de proyecto</attr>
+        /// <attr name="_entrega">Dependiendo del compromiso de pago se llegara a una cantidad de entregas estipuladas </attr>
+        /// <attr name="_feincio">fecha estimada de inicio </attr>
+        /// <attr name="_fefinal">fecha estimada de fin </attr>
         /// <attr name="costo">Costo de realizacion del 100% del proyecto</attr>
-        /// <attr name="estatus">Estado en el que se encuentra la propuesta {Aprobada, Pendiente, Cerrada}</attr>
+        /// <attr name="_listaCompania">lista de las compañias de las cuales se puede generar una propuesta</attr> 
+        /// <attr name="_listaRequerimiento">lista de requerimientos asociados a un proyecto</attr> 
         /// </summary>
 
         private String _codigo;
@@ -29,17 +33,27 @@ namespace DominioTangerine
         private String _duracion;
         private String _acuerdopago;
         private String _estatus;
+        private String _moneda;
         private int _entrega;
         private DateTime _feincio;
         private DateTime _fefinal;
-        private String _moneda;
         private int _costo;
 
         private List<Compania> _listaCompania;
         private List<Requerimiento> _listaRequerimiento;
-      
+
         #endregion
 
-      
+        #region Constructor
+
+        public Propuesta()
+        {
+
+        }
+
+        #endregion
+
+
+
     }
 }
