0b2e306851cb29f524bc10879ebb6f181c3ee50e
ieba <istvanbokor8@gmail.com>
2016-05-10 23:03:23 -0400

    Boton en consultar para agregarcontacto

--- Tangerine/Tangerine/GUI/M5/ConsultarContactos.aspx.designer.cs
+++ Tangerine/Tangerine/GUI/M5/ConsultarContactos.aspx.designer.cs
@@ -31,10 +31,12 @@ namespace Tangerine.GUI.M5 {
         protected global::System.Web.UI.WebControls.Literal tabla;
         
         /// <summary>
+        /// nuevocontacto control.
         /// </summary>
         /// <remarks>
         /// Auto-generated field.
         /// To modify move field declaration from designer file to code-behind file.
         /// </remarks>
+        protected global::System.Web.UI.WebControls.Literal nuevocontacto;
     }
 }
