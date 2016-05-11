b5d307bf089012a26f62e9b45fd981ba301be270
Daniel Da Mata <Dani7.da.mata@gmail.com>
2016-05-10 20:27:54 -0430

    comit

--- Tangerine/Tangerine/DatosTangerine/M7/BDEmpleadoProyecto.cs
+++ Tangerine/Tangerine/DatosTangerine/M7/BDEmpleadoProyecto.cs
@@ -17,39 +17,39 @@ namespace DatosTangerine.M7
         List<Parametro> parameters;
         Parametro theParam = new Parametro();
 
-        public Boolean AddProyecto(Proyecto TheProyecto)
+        public Boolean AddProyectoEmpleado (Proyecto TheProyecto)
         {
-            parameters = new List<Parametro>();
-            theConnection = new BDConexion();
+            /*  parameters = new List<Parametro>();
+              theConnection = new BDConexion();
 
-            for (int i = 0; i < TheProyecto.get_empleados().Count(); i++)
-            {
-                try
-                {
-                    //Las dos lineas siguientes tienen que repetirlas tantas veces como parametros reciba su stored procedure a llamar
-                    //Parametro recibe (nombre del primer parametro en su stored procedure, el tipo de dato, el valor, false)
-
-
-
-                    theParam = new Parametro(ResourceProyecto.ParamId_Proyecto, SqlDbType.Int, TheProyecto.Idproyecto.ToString(), false);
-                    parameters.Add(theParam);
-
-                    theParam = new Parametro(ResourceProyecto.ParamNombre, SqlDbType.Int, TheProyecto.get_empleados()[i], false);
-                    parameters.Add(theParam);
+              for (int i = 0; i < TheProyecto.get_empleados().Count(); i++)
+              {
+                  try
+                  {
+                      //Las dos lineas siguientes tienen que repetirlas tantas veces como parametros reciba su stored procedure a llamar
+                      //Parametro recibe (nombre del primer parametro en su stored procedure, el tipo de dato, el valor, false)
 
 
 
-                    //Se manda a ejecutar en BDConexion el stored procedure M7_AgregarProyecto y todos los parametros que recibe
-                    List<Resultado> results = theConnection.EjecutarStoredProcedure(ResourceProyecto.AddNewProyecto, parameters);
+                      theParam = new Parametro(ResourceProyecto.ParamId_Proyecto, SqlDbType.Int, TheProyecto.Idproyecto.ToString(), false);
+                      parameters.Add(theParam);
 
-                }
-                catch (Exception ex)
-                {
-                    throw new ExcepcionesTangerine.ExceptionsTangerine(RecursoGeneralBD.Mensaje_Generico_Error, ex);
-                }
+                      theParam = new Parametro(ResourceProyecto.ParamNombre, SqlDbType.Int, TheProyecto.get_empleados()[i], false);
+                      parameters.Add(theParam);
 
-            }
-                 return true;
+
+
+                      //Se manda a ejecutar en BDConexion el stored procedure M7_AgregarProyecto y todos los parametros que recibe
+                      List<Resultado> results = theConnection.EjecutarStoredProcedure(ResourceProyecto.AddNewProyecto, parameters);
+
+                  }
+                  catch (Exception ex)
+                  {
+                      throw new ExcepcionesTangerine.ExceptionsTangerine(RecursoGeneralBD.Mensaje_Generico_Error, ex);
+                  }
+
+              }*/
+            return true;
         }
     }
 }
