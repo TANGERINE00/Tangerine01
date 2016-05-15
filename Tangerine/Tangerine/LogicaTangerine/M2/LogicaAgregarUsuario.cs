5ebfc0265b49cb921141604433e361706e5b69b1
luarropa <lrodriguezp.18@gmail.com>
2016-05-14 22:20:46 -0400

    agregar

--- Tangerine/Tangerine/LogicaTangerine/M2/LogicaAgregarUsuario.cs
+++ Tangerine/Tangerine/LogicaTangerine/M2/LogicaAgregarUsuario.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DatosTangerine.M2;
+using DominioTangerine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -8,5 +10,13 @@ namespace LogicaTangerine.M2
 {
     public class LogicaAgregarUsuario
     {
+        public static bool AgregarUsuario( Usuario usuario ) 
+        {
+            bool resultado = false;
+
+            resultado = BDUsuario.AgregarUsuario( usuario );
+
+            return resultado;
+        }
     }
 }
