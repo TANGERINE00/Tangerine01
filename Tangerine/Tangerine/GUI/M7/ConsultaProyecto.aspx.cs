480d8ff449311522f70a35f24158e9a3a781f3b7
omfigueira <omfr.13@gmail.com>
2016-05-16 18:29:39 -0400

    Consultar

--- Tangerine/Tangerine/GUI/M7/ConsultaProyecto.aspx.cs
+++ Tangerine/Tangerine/GUI/M7/ConsultaProyecto.aspx.cs
@@ -4,14 +4,35 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
+using LogicaTangerine;
+using LogicaTangerine.M10;
+using LogicaTangerine.M6;
+using LogicaTangerine.M4;
+using LogicaTangerine.M7;
 
 namespace Tangerine.GUI.M7
 {
     public partial class WebForm3 : System.Web.UI.Page
     {
+
+        public string proyecto
+        {
+            get
+            {
+                return this.tabla.Text;
+            }
+
+            set
+            {
+                this.tabla.Text = value;
+            }
+        }
+
         protected void Page_Load(object sender, EventArgs e)
         {
-
+            LogicaProyecto pruebaM7 = new LogicaProyecto();
+            LogicaM4 pruebaM4 = new LogicaM4();
+            LogicaPropuesta pruebaM6 = new LogicaPropuesta();
         }
     }
 }
