3a2952d505c9bc984603d01b87b9fc3c95fe6f8e
EddCold <ejpacheco.11@est.ucab.edu.ve>
2016-05-18 17:41:52 -0400

    M6_Cambios
    cambios agregar

---
+++ Tangerine/Tangerine/LogicaTangerine/M6/LogicaRequerimiento.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatosTangerine.M6;
+using DominioTangerine;
+
+namespace LogicaTangerine.M6
+{
+    {
+
+        /// <summary>
+        /// Metodo para agregar un requerimiento a su respcetiva propuesta.
+        /// </summary>
+        /// <param name="propuesta">objeto de tipo Requerimiento para agregar</param>
+        /// <returns>true si fue agregado</returns>
+        public bool agregar(Requerimiento requerimiento)
+        {
+        }
+
+      
+
+    }
+}
