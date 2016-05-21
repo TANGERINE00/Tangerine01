6f9428404a3005c730751b158043f261b4a346c9
Eduardo Pacheco <arzagoth@gmail.com>
2016-05-21 08:53:36 -0400

    M6_arreglo

--- Tangerine/Tangerine/DatosTangerine/M6/BDPropuesta.cs
+++ Tangerine/Tangerine/DatosTangerine/M6/BDPropuesta.cs
@@ -150,7 +150,7 @@ namespace DatosTangerine.M6
                 //Por cada fila de la tabla voy a guardar los datos 
                 foreach (DataRow row in dt.Rows)
                 {
-                    
+                    String codigonumP = row[RecursosPropuesta.PropCodigo].ToString();
                     String conNombre = row[RecursosPropuesta.PropNombre].ToString();
                     String conDescripcion = row[RecursosPropuesta.PropDescripcion].ToString();
                     String contipoDuracion = row[RecursosPropuesta.PropTipoDuracion].ToString();
@@ -167,7 +167,7 @@ namespace DatosTangerine.M6
 
                       
                     //Creo un objeto de tipo Propuesta con los datos de la fila y lo guardo en una lista de propuestas
-                    Propuesta propuestas = new Propuesta(conNombre, conDescripcion, contipoDuracion, conDuracion,
+                    Propuesta propuestas = new Propuesta(codigonumP,conNombre, conDescripcion, contipoDuracion, conDuracion,
                         conAcuerdo, conEstatus, conMoneda, conEntregas, conFechaIni, conFechaFin, conCosto, conFkComp);
                         listaPropuesta.Add(propuestas);
                 }
