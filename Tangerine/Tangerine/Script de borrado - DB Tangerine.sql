e57aafce129c3c91b6f9cd342b0382e054c13dad
daniell21 <alc_daniel_l@hotmail.com>
2016-05-22 16:21:37 -0400

    Revert "Agregar Procedure al Script"
    This reverts commit 1030502db27fc15100e15ea12baf5c2eb2962557.

--- Tangerine/Tangerine/Script de borrado - DB Tangerine.sql
+++ Tangerine/Tangerine/Script de borrado - DB Tangerine.sql
@@ -96,7 +96,6 @@ drop procedure M8_VerificarFacturasParaPagar;
 drop procedure M8_ConsultarMontoRestanteFactura;
 
 drop procedure M9_AgregarPago;
-drop procedure M9_CambioStatus;
 
 drop procedure M10_AgregarEmpleado;
 drop procedure M10_ConsultarEmpleado;
