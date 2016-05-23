0fc4f948fcd79a3329cc218819b35c6b172e748b
Luis Rodriguez <lrodriguezp.18@gmail.com>
2016-05-22 22:13:11 -0400

    Añadir comentarios M2

--- Tangerine/Tangerine/ExcepcionesTangerine/M2/ExcepcionModificarRol.cs
+++ Tangerine/Tangerine/ExcepcionesTangerine/M2/ExcepcionModificarRol.cs
@@ -8,29 +8,54 @@ namespace ExcepcionesTangerine.M2
 {
     public class ExcepcionModificarRol : ExceptionsTangerine
     {
+        /// <summary>
+        /// Contructor por defecto de la clase
+        /// </summary>
         public ExcepcionModificarRol()
         {
 
         }
+
+        /// <summary>
+        /// Constructor que recibe el mensaje que se quiere mostrar
+        /// </summary>
+        /// <param name="message"></param>
         public ExcepcionModificarRol( string message )
             : base ( message )
         {
 
         }
 
-        public ExcepcionModificarRol( string message, Exception inner)
+        /// <summary>
+        /// Constructor que recibe el mensaje y la excepcion que se produjo
+        /// </summary>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public ExcepcionModificarRol( string message, Exception inner )
             : base( message, inner )
         {
 
         }
-        public ExcepcionModificarRol(string codigo, string message, Exception inner)
-            : base(codigo, message, inner)
+
+        /// <summary>
+        /// Constructor que recibe el codigo, el mensaje y la excepcion que se produjo
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
+        public ExcepcionModificarRol( string codigo, string message, Exception inner )
+            : base( codigo, message, inner )
         {
 
         }
+
+        /// <summary>
+        /// Sobrecarga del metodo ToString()
+        /// </summary>
+        /// <returns></returns>
         public override string ToString()
         {
-            return string.Format("[ExcepcionModificarRol: (Mensaje = {0}) (Excepción={1})]", Mensaje, Excepcion);
+            return string.Format( "[ExcepcionModificarRol: (Mensaje = {0}) (Excepción={1})]", Mensaje, Excepcion );
         }
     }
 }
