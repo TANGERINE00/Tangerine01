83d50019448740099e3a41c5d6a88454485760f9
Paul Yelamo <paulgyg91@gmail.com>
2016-05-22 21:48:37 -0430

    Creacion Nueva Vista M9 - consultar pagos

--- Tangerine/Tangerine/GUI/Master/Tangerine.Master.designer.cs
+++ Tangerine/Tangerine/GUI/Master/Tangerine.Master.designer.cs
@@ -238,6 +238,15 @@ namespace Tangerine.GUI.Master {
         protected global::System.Web.UI.HtmlControls.HtmlGenericControl Cargarpago;
         
         /// <summary>
+        /// Li1 control.
+        /// </summary>
+        /// <remarks>
+        /// Auto-generated field.
+        /// To modify move field declaration from designer file to code-behind file.
+        /// </remarks>
+        protected global::System.Web.UI.HtmlControls.HtmlGenericControl Li1;
+        
+        /// <summary>
         /// GestionDeEmpleados control.
         /// </summary>
         /// <remarks>
