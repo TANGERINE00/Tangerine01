92ed300b9a2e800e974af67fd7f4dc25b6b27c1d
Gianfranco Berlino <giantufano@gmail.com>
2016-06-11 10:09:31 -0400

    Dominio Tangerine. Clase Rol

---
+++ Tangerine/Tangerine/DominioTangerine/Entidades/M2/Rol.cs
@@ -0,0 +1,89 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioTangerine.Entidades.M2
+{
+    public class Rol
+    {
+        #region Atributos
+
+        private string _nombre;
+        private ListaGenerica<Menu> _menu;
+
+        #endregion
+
+        #region Constructores
+
+        /// <summary>
+        /// Constructor vacío de la clase Rol
+        /// </summary>
+        public Rol()
+        {
+        }
+
+        /// <summary>
+        /// Constructor de la clase Rol con nombre
+        /// </summary>
+        /// <param name="nombre"></param>
+        public Rol(string nombre)
+        {
+            _nombre = nombre;
+            _menu = new ListaGenerica<Menu>();
+        }
+
+        /// <summary>
+        /// Constructor de la clase Rol con todos sus atributos
+        /// </summary>
+        /// <param name="nombre"></param>
+        /// <param name="menus"></param>
+        public Rol(string nombre, ListaGenerica<Menu> menu)
+            :this(nombre)
+        {
+            _menu = menu;
+        }
+
+        #endregion
+
+        #region Get's y Set's
+
+        /// <summary>
+        /// Get y Set del atributo _nombre
+        /// </summary>
+        /// <returns>Nombre del rol</returns>
+        public string nombre
+        {
+            get { return _nombre; }
+            set { _nombre = value; }
+        }
+
+        /// <summary>
+        /// Get y Set de la lista de menu
+        /// </summary>
+        /// <returns>Lista de los menus que continen las opciones a las que no puede acceder el rol</returns>
+        public ListaGenerica<Menu> menu
+        {
+            get { return _menu; }
+            set { _menu = value; }
+        }
+
+        #endregion
+
+        #region Métodos
+
+        /// <summary>
+        /// Método para imprimir los datos actuales de la lista
+        /// </summary>
+        public void imprimirListaDeMenu()
+        {
+            foreach (Menu m in _menu)
+            {
+                System.Diagnostics.Debug.WriteLine("Menú: " + m.Nombre);
+            }
+        }
+
+        #endregion
+    }
+}
--- Tangerine/Tangerine/DominioTangerine/Entidades/M2/Usuario.cs
+++ Tangerine/Tangerine/DominioTangerine/Entidades/M2/Usuario.cs
@@ -7,7 +7,7 @@ using System.Security.Cryptography;
 
 namespace DominioTangerine.Entidades.M2
 {
-    class Usuario
+    public class Usuario
     {
         #region Atributos
 
