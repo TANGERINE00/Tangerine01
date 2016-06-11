9f519234149da972d5a750b2fb80be99d69a1ab5
jmri1508 <jrirausquin.1995@gmail.com>
2016-06-11 09:31:50 -0430

    Estructura Dao m4 con su respectiva fabrica

---
+++ Tangerine/Tangerine/DatosTangerine/DAO/M4/DaoCompania.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatosTangerine.InterfazDAO.M4;
+using DominioTangerine.Entidades.M4;
+
+namespace DatosTangerine.DAO.M4
+{
+    public class DaoCompania : DAOGeneral, IDaoCompania
+    {
+       public int ConsultLastCompanyId()
+        {
+            return 0;
+        }
+
+       public bool DeleteCompany(CompaniaM4 theCompany)
+       {
+           return true;
+       }
+       public bool EnableCompany(CompaniaM4 theCompany)
+       {
+           return true;
+       }
+       public bool DisableCompany(CompaniaM4 theCompany)
+       {
+           return true;
+       }
+    }
+}
---
+++ Tangerine/Tangerine/DatosTangerine/DAO/M4/DaoLugarDireccion.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatosTangerine.InterfazDAO.M4;
+using DominioTangerine.Entidades.M4;
+
+namespace DatosTangerine.DAO.M4
+{
+    public class DaoLugarDireccion : DAOGeneral,IDaoLugarDireccion
+    {
+       public List<LugarDireccionM4> ConsultCityPlaces()
+        {
+            return new List<LugarDireccionM4>();
+        }
+    }
+}
