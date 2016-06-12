a032ca99ab2260cae2c6d35b66f2c1ab3f0bf4a7
Javier Merchán <merchan1395@gmail.com>
2016-06-11 20:32:03 -0400

    DAO + Dominio (entidades) - M6 Propuesta
    .

---
+++ Tangerine/Tangerine/DatosTangerine/DAO/M6/DAOPropuesta.cs
@@ -0,0 +1,120 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatosTangerine.InterfazDAO.M6;
+using DominioTangerine.Entidades.M6;
+
+namespace DatosTangerine.DAO.M6
+{
+    public class DAOPropuesta : DAOGeneral, IDAOPropuesta
+    {
+        /// <summary>
+        /// Metodo para agregar una propuesta en la base de datos.
+        /// </summary>
+        ///  <param name="parametro">objeto de tipo Propuesta para agregar en BD</param>
+        ///  <param name="theConnection">Objeto de tipo BDConexion para la conexion a la BD</param>
+        ///  <param name="parametros">objeto de tipo lista parametro para la captura de los campos</param>
+        /// <returns>true si fue agregado</returns>
+        public static bool agregarPropuesta(Propuesta laPropuesta)
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// Metodo para agregar un Requerimiento en la base de datos.
+        /// </summary>
+        /// <param name="parametro">objeto de tipo Requerimiento para agregar en bd</param>
+        /// <returns>true si fue agregado</returns>
+        public static bool agregarRequerimiento(Requerimiento elRequerimiento)
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// Metodo diseñado para M7, que devuelve la lista de propuestas con estatus aprobado y que no están en proyecto
+        /// </summary>
+        /// <returns>Retorna la lista de propuestas con estatus= Aprobado y que no se encuentran aún en Proyecto</returns>
+        public List<Propuesta> PropuestaProyecto()
+        {
+            List<Propuesta> listaPropuesta = new List<Propuesta>();
+
+            return listaPropuesta;
+        }
+
+
+        /// <summary>
+        /// Metodo que consulta todas las propuestas
+        /// </summary>
+        ///
+        /// <returns>Retorna la lista de propuestas</returns>
+        public static List<Propuesta> ListarLasPropuestas()
+        {
+            List<Propuesta> listaPropuesta = new List<Propuesta>();
+            
+            return listaPropuesta;
+        }
+
+
+        /// <summary>
+        /// Método para listar los requerimientos por propuesta 
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static List<Requerimiento> ConsultarRequerimientosPorPropuesta(String id)
+        {
+            List<Requerimiento> listaRequerimientos = new List<Requerimiento>();
+            return listaRequerimientos;
+        }
+
+
+        /// <summary>
+        /// Metodo para consultar propuesta por id (nombre)
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        public static Propuesta ConsultarPropuestaporNombre(String id)
+        {
+            Propuesta propuesta = null;
+
+            return propuesta;
+        }
+
+
+        /// <summary>
+        /// Metodo para eliminar una Propuesta de la base de datos.
+        /// </summary>
+        /// <param name="parametro">objeto de tipo Contacto a eliminar en bd</param>
+        /// <returns>true si fue eliminado</returns>
+        public static Boolean BorrarPropuesta(string nombrePropuesta)
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// Metodo para modificar un requerimiento
+        /// </summary>
+        /// <param name="elrequerimiento"></param>
+        /// <returns></returns>
+        public static Boolean Modificar_Requerimiento(Requerimiento elrequerimiento)
+        {
+            return true;
+        }
+
+
+        /// <summary>
+        /// Metodo que permite modificar una propuesta en la BD
+        /// </summary>
+        /// <param name="propuesta"></param>
+        /// <returns></returns>
+        public static Boolean Modificar_Propuesta(Propuesta propuesta)
+        {
+            return true;
+        }
+
+    }
+}
---
+++ Tangerine/Tangerine/DominioTangerine/Entidades/M6/Requerimiento.cs
@@ -0,0 +1,66 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DominioTangerine.Entidades.M6
+{
+    public class Requerimiento : Entidad
+    {
+        #region Atributos
+
+        /// <summary>
+        /// Clase Requerimiento
+        /// <attr name="_codigoRG">Codigo unico indentificador del requerimiento asociado a una propuesta</attr>
+        /// <attr name="_descripcion">breve descripcion del requerimientos</attr>
+        /// </summary>
+
+        private string _codigoRequerimiento;
+        private string _descripcion;
+        private string _codigoPropuesta;
+        private int p1;
+        private string p2;
+        private string p3;
+        #endregion
+   
+        #region Propiedades
+
+        public string CodigoRequerimiento
+        {
+            get { return _codigoRequerimiento; }
+            set { _codigoRequerimiento = value; }
+        }
+      
+        public string Descripcion
+        {
+            get { return _descripcion; }
+            set { _descripcion = value; }
+        }
+
+        public string CodigoPropuesta
+        {
+            get { return _codigoPropuesta; }
+            set { _codigoPropuesta = value; }
+        }
+
+        #endregion
+
+        #region Constructor
+
+        public Requerimiento()
+        {
+
+        }
+
+        public Requerimiento(string codreq, string descripr,string codpro)
+        {
+            this._codigoRequerimiento = codreq;
+            this._descripcion = descripr;
+            this._codigoPropuesta = codpro;
+        }
+
+        #endregion
+    
+    }
+}
