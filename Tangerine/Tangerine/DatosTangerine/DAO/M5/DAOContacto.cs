f05ac75d5420da0e6b8a0551e8f947cf6051aed7
Luis Rodriguez <lrodriguezp.18@gmail.com>
2016-06-14 00:22:43 -0400

    dao m5

--- Tangerine/Tangerine/DatosTangerine/DAO/M5/DAOContacto.cs
+++ Tangerine/Tangerine/DatosTangerine/DAO/M5/DAOContacto.cs
@@ -1,4 +1,6 @@
-﻿using System;
+﻿using DatosTangerine.InterfazDAO.M5;
+using DominioTangerine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -6,8 +8,51 @@ using System.Threading.Tasks;
 
 namespace DatosTangerine.DAO.M5
 {
-    class DAOContacto
+    class DAOContacto : DAOGeneral, IDAOContacto
     {
+        public bool Agregar(Entidad contacto)
+        {
+            return true;
+        }
 
+        public bool Eliminar(Entidad contacto)
+        {
+            return true;
+        }
+
+        public bool Modificar(Entidad contacto)
+        {
+            return true;
+        }
+
+        public Entidad ConsultarXId(Entidad contacto)
+        {
+            return contacto;
+        }
+
+        public List<Entidad> ContactosPorCompania(Entidad compania)
+        {
+            return new List<Entidad>();
+        }
+
+        public bool AgregarContactoAProyecto(Entidad contacto, Entidad proyecto)
+        {
+            return true;
+        }
+
+        public List<Entidad> ContactosPorProyecto(Entidad proyecto)
+        {
+            return new List<Entidad>();
+        }
+
+        public bool EliminarContactoDeProyecto(Entidad contacto, Entidad proyecto)
+        {
+            return true;
+        }
+
+        public List<Entidad> ContactosNoPertenecenAProyecto(Entidad proyecto)
+        {
+            return new List<Entidad>();
+        }
     }
 }
