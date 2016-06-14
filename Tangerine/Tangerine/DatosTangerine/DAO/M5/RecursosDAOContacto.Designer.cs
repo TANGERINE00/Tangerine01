21cbcda380ea972881e1ec4406d70227c7dde5a0
Luis Rodriguez <lrodriguezp.18@gmail.com>
2016-06-14 19:06:24 -0400

    recurso contacto dao m5

--- Tangerine/Tangerine/DatosTangerine/DAO/M5/RecursosDAOContacto.Designer.cs
+++ Tangerine/Tangerine/DatosTangerine/DAO/M5/RecursosDAOContacto.Designer.cs
@@ -59,5 +59,302 @@ namespace DatosTangerine.DAO.M5 {
                 resourceCulture = value;
             }
         }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a M5_AgregarContacto.
+        /// </summary>
+        internal static string AgregarContacto {
+            get {
+                return ResourceManager.GetString("AgregarContacto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a M5_AgregarContactoProyecto.
+        /// </summary>
+        internal static string AgregarContactoProyecto {
+            get {
+                return ResourceManager.GetString("AgregarContactoProyecto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a TGE_00_001.
+        /// </summary>
+        internal static string Codigo_Error_Formato {
+            get {
+                return ResourceManager.GetString("Codigo_Error_Formato", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a con_apellido.
+        /// </summary>
+        internal static string ConApellidoContacto {
+            get {
+                return ResourceManager.GetString("ConApellidoContacto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a con_cargo.
+        /// </summary>
+        internal static string ConCargoContacto {
+            get {
+                return ResourceManager.GetString("ConCargoContacto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a con_correo.
+        /// </summary>
+        internal static string ConCorreo {
+            get {
+                return ResourceManager.GetString("ConCorreo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a con_departamento.
+        /// </summary>
+        internal static string ConDepartamentoContacto {
+            get {
+                return ResourceManager.GetString("ConDepartamentoContacto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a fk_id_com_lead.
+        /// </summary>
+        internal static string ConIdCompania {
+            get {
+                return ResourceManager.GetString("ConIdCompania", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a con_id.
+        /// </summary>
+        internal static string ConIdContacto {
+            get {
+                return ResourceManager.GetString("ConIdContacto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a con_nombre.
+        /// </summary>
+        internal static string ConNombreContacto {
+            get {
+                return ResourceManager.GetString("ConNombreContacto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a M5_ConsultarContactoCompania.
+        /// </summary>
+        internal static string ConsultarContactoCompania {
+            get {
+                return ResourceManager.GetString("ConsultarContactoCompania", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a M5_ConsultarContactoId.
+        /// </summary>
+        internal static string ConsultarContactoId {
+            get {
+                return ResourceManager.GetString("ConsultarContactoId", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a M5_ConsultarContactoNoProyecto.
+        /// </summary>
+        internal static string ConsultarContactoNoProyecto {
+            get {
+                return ResourceManager.GetString("ConsultarContactoNoProyecto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a M5_ConsultarContactoProyecto.
+        /// </summary>
+        internal static string ConsultarContactoProyecto {
+            get {
+                return ResourceManager.GetString("ConsultarContactoProyecto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a con_telefono.
+        /// </summary>
+        internal static string ConTelefono {
+            get {
+                return ResourceManager.GetString("ConTelefono", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a con_tipo_emp.
+        /// </summary>
+        internal static string ConTipoCompania {
+            get {
+                return ResourceManager.GetString("ConTipoCompania", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a M5_EliminarContacto.
+        /// </summary>
+        internal static string EliminarConacto {
+            get {
+                return ResourceManager.GetString("EliminarConacto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a M5_EliminarContactoProyecto.
+        /// </summary>
+        internal static string EliminarContactoProyecto {
+            get {
+                return ResourceManager.GetString("EliminarContactoProyecto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Error de parseo.
+        /// </summary>
+        internal static string Mensaje_Error_Formato {
+            get {
+                return ResourceManager.GetString("Mensaje_Error_Formato", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Salida del Método.
+        /// </summary>
+        internal static string MensajeFinInfoLogger {
+            get {
+                return ResourceManager.GetString("MensajeFinInfoLogger", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a Entrada en el Método.
+        /// </summary>
+        internal static string MensajeInicioInfoLogger {
+            get {
+                return ResourceManager.GetString("MensajeInicioInfoLogger", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a M5_ModificarContacto.
+        /// </summary>
+        internal static string ModificarContacto {
+            get {
+                return ResourceManager.GetString("ModificarContacto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @apellido.
+        /// </summary>
+        internal static string ParametroApellido {
+            get {
+                return ResourceManager.GetString("ParametroApellido", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @cargo.
+        /// </summary>
+        internal static string ParametroCargo {
+            get {
+                return ResourceManager.GetString("ParametroCargo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @correo.
+        /// </summary>
+        internal static string ParametroCorreo {
+            get {
+                return ResourceManager.GetString("ParametroCorreo", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @departamento.
+        /// </summary>
+        internal static string ParametroDepartamento {
+            get {
+                return ResourceManager.GetString("ParametroDepartamento", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @id.
+        /// </summary>
+        internal static string ParametroId {
+            get {
+                return ResourceManager.GetString("ParametroId", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @id_empresa.
+        /// </summary>
+        internal static string ParametroIdCompania {
+            get {
+                return ResourceManager.GetString("ParametroIdCompania", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @id_contacto.
+        /// </summary>
+        internal static string ParametroIdContacto {
+            get {
+                return ResourceManager.GetString("ParametroIdContacto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @id_proyecto.
+        /// </summary>
+        internal static string ParametroIdProyecto {
+            get {
+                return ResourceManager.GetString("ParametroIdProyecto", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @nombre.
+        /// </summary>
+        internal static string ParametroNombre {
+            get {
+                return ResourceManager.GetString("ParametroNombre", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @telefono.
+        /// </summary>
+        internal static string ParametroTelefono {
+            get {
+                return ResourceManager.GetString("ParametroTelefono", resourceCulture);
+            }
+        }
+        
+        /// <summary>
+        ///   Busca una cadena traducida similar a @tipo_comp.
+        /// </summary>
+        internal static string ParametroTipoCompania {
+            get {
+                return ResourceManager.GetString("ParametroTipoCompania", resourceCulture);
+            }
+        }
     }
 }
