0ebb1a3500fa663c2b0237fa40945292f11aad58
Javier Merchán <merchan1395@gmail.com>
2016-06-15 14:52:38 -0400

    DAO M6 - Comandos
    AgregarRequerimiento

--- Tangerine/Tangerine/LogicaTangerine/Comandos/M6/ComandoAgregarRequerimiento.cs
+++ Tangerine/Tangerine/LogicaTangerine/Comandos/M6/ComandoAgregarRequerimiento.cs
@@ -3,10 +3,29 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using DatosTangerine.InterfazDAO.M6;
+using DominioTangerine;
 
 namespace LogicaTangerine.Comandos.M6
 {
-    class ComandoAgregarRequerimiento
+    public class ComandoAgregarRequerimiento: Comando<bool>
     {
+
+        public ComandoAgregarRequerimiento(Entidad elRequerimiento) {
+             _laEntidad = elRequerimiento;
+        }
+
+        public override bool Ejecutar()
+        {
+            try
+            {
+                IDAORequerimiento daoRequerimiento = DatosTangerine.Fabrica.FabricaDAOSqlServer.CrearDAORequerimiento();
+                return daoRequerimiento.Agregar(_laEntidad);
+            }
+            catch (Exception e)
+            {
+                throw e;
+            }
+        }
     }
 }
