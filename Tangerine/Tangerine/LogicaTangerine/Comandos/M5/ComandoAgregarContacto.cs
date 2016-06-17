cef62f1f992d16ef75308178b28ef671b5e80587
Luis Rodriguez <lrodriguezp.18@gmail.com>
2016-06-17 18:35:32 -0400

    avance comandos m5

---
+++ Tangerine/Tangerine/LogicaTangerine/Comandos/M5/ComandoAgregarContacto.cs
@@ -0,0 +1,22 @@
+﻿using DominioTangerine;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogicaTangerine.Comandos.M5
+{
+    public class ComandoAgregarContacto : Comando<bool>
+    {
+        public ComandoAgregarContacto( Entidad contacto ) 
+        {
+            _laEntidad = contacto;
+        }
+
+        public override bool Ejecutar()
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
