96d2b2e653d50cb24952397dbed5f2438a2a04a5
Fernando Silva <fernandojsc@outlook.com>
2016-06-18 15:56:01 -0400

    Adelantos presentador M6
    Adelantos presentadores agregarpropuesta y eliminarpropuesta.

--- Tangerine/Tangerine/Tangerine_Presentador/M6/PresentadorAgregarPropuesta.cs
+++ Tangerine/Tangerine/Tangerine_Presentador/M6/PresentadorAgregarPropuesta.cs
@@ -1,12 +1,31 @@
-﻿using System;
+﻿using DatosTangerine;
+using DominioTangerine;
+using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using Tangerine_Contratos.M6;
+using LogicaTangerine;
+using DominioTangerine.Entidades.M6;
 
 namespace Tangerine_Presentador.M6
 {
+
     class PresentadorAgregarPropuesta
     {
+        IContratoAgregarPropuesta vista;
+        Boolean Confirmacion;
+
+        public PresentadorAgregarPropuesta(IContratoAgregarPropuesta vista)
+        {
+            this.vista = vista;
+        }
+        public void agregarPropuesta()
+        {
+            DominioTangerine.Entidades.M6.Propuesta p = new DominioTangerine.Entidades.M6.Propuesta();
+            LogicaTangerine.Comando<bool> comando = LogicaTangerine.Fabrica.FabricaComandos.ComandoAgregarPropuesta(p);
+            Confirmacion = comando.Ejecutar();
+        }
     }
 }
