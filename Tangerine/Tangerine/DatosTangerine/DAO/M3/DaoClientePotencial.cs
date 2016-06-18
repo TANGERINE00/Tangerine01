70e71f73a8dd379a11883540fe37dc5073f24882
toniojua <antonio11346@gmail.com>
2016-06-18 15:02:30 -0430

    M3 dao lead consultar por id

--- Tangerine/Tangerine/DatosTangerine/DAO/M3/DaoClientePotencial.cs
+++ Tangerine/Tangerine/DatosTangerine/DAO/M3/DaoClientePotencial.cs
@@ -1,5 +1,6 @@
 ﻿using DatosTangerine.InterfazDAO.M3;
 using DatosTangerine.M3;
+using DominioTangerine.Entidades.M3;
 using DominioTangerine;
 using System;
 using System.Collections.Generic;
@@ -30,7 +31,86 @@ namespace DatosTangerine.DAO.M3
 
         public Entidad ConsultarXId(Entidad parametro)
         {
-            throw new NotImplementedException();
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+                ResourceClientePotencial.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+            
+            //QUITAR DOMINIOTANGERINE AQUI Y EN EL RESTO DEL CODIGO LUEGO DE BORRAR CLIENTE POTENCIAL DEL DOMINIO
+            DominioTangerine.Entidades.M3.ClientePotencial cliente = (DominioTangerine.Entidades.M3.ClientePotencial)parametro;
+            List<Parametro> parameters = new List<Parametro>();
+            BDConexion theConnection = new BDConexion();
+            Parametro theParam = new Parametro();
+
+            List<DominioTangerine.Entidades.M3.ClientePotencial> listClientePotencial = new List<DominioTangerine.Entidades.M3.ClientePotencial>();
+            DominioTangerine.Entidades.M3.ClientePotencial elClientePotencial = null;
+
+            try
+            {
+                theConnection.Conectar();
+
+                theParam = new Parametro(ResourceClientePotencial.AidClientePotencial, SqlDbType.Int,
+                cliente.Id.ToString(), false);
+                parameters.Add(theParam);
+
+                //Guardo la tabla que me regresa el procedimiento de consultar contactos
+                DataTable dt = theConnection.EjecutarStoredProcedureTuplas(ResourceClientePotencial.SP_consultarClientePotencial, parameters);
+
+                //Por cada fila de la tabla voy a guardar los datos 
+                foreach (DataRow row in dt.Rows)
+                {
+
+                    int IdClientePotencial = int.Parse(row[ResourceClientePotencial.idClientePotencial].ToString());
+                    String NombreClientePotencial = row[ResourceClientePotencial.nombreClientePotencial].ToString();
+                    String RifClientePotencial = row[ResourceClientePotencial.rifClientePotencial].ToString();
+                    String EmailClientePotencial = row[ResourceClientePotencial.emailClientePotencial].ToString();
+                    float PresupuestoAnual_inversion = float.Parse(row[ResourceClientePotencial.presupuestoAnual_inversion].ToString());
+                    //String PresupuestoAnual_inversion = row[ResourceClientePotencial.emailClientePotencial].ToString();
+                    int NumeroLlamadas = int.Parse(row[ResourceClientePotencial.numeroLlamadas].ToString());
+                    int NumeroVisitas = int.Parse(row[ResourceClientePotencial.numeroVisitas].ToString());
+                    int Status = int.Parse(row[ResourceClientePotencial.status].ToString());
+                    //  int conCompId = int.Parse(row[ResourceClientePotencial.ConIdComp].ToString());
+
+                    //Creo un objeto de tipo Contacto con los datos de la fila y lo guardo en una lista de contactos
+                    elClientePotencial = new DominioTangerine.Entidades.M3.ClientePotencial(IdClientePotencial, NombreClientePotencial, RifClientePotencial,
+                        EmailClientePotencial, PresupuestoAnual_inversion, NumeroLlamadas, NumeroVisitas, Status);
+
+                }
+
+            }
+            catch (ArgumentNullException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw new ExcepcionesTangerine.M3.NullArgumentExceptionLeads(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }
+            catch (SqlException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw new ExcepcionesTangerine.ExceptionTGConBD(RecursoGeneralBD.Codigo,
+                    RecursoGeneralBD.Mensaje, ex);
+            }
+            catch (FormatException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExcepcionesTangerine.M3.WrongFormatExceptionLeads(ResourceClientePotencial.Codigo_Error_Formato,
+                    ResourceClientePotencial.Mensaje_Error_Formato, ex);
+            }
+            catch (ExcepcionesTangerine.ExceptionTGConBD ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw ex;
+            }
+            catch (Exception ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExcepcionesTangerine.ExceptionsTangerine(RecursoGeneralBD.Mensaje_Generico_Error, ex);
+            }
+            Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+                ResourceClientePotencial.MensajeFinInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
+
+            return elClientePotencial;
         }
 
         public List<Entidad> ConsultarTodos()
