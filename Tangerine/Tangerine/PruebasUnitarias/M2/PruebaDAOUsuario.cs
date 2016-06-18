e8cc2d6dfb7583acb0d68464e3d2b37b579a8106
daniell21 <alc_daniel_l@hotmail.com>
2016-06-18 18:41:14 -0400

    Pruebas Unitarias M2

---
+++ Tangerine/Tangerine/PruebasUnitarias/M2/PruebaDAOUsuario.cs
@@ -0,0 +1,84 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using DatosTangerine.DAO.M2;
+using DatosTangerine.InterfazDAO.M2;
+using DominioTangerine;
+using NUnit.Framework;
+using DominioTangerine.Entidades.M2;
+
+namespace PruebasUnitarias.M2
+{
+    [TestFixture]
+    class PruebaDAOUsuario
+    {
+        #region Atributos
+        public bool answer;
+        //public bool answer1;
+        public RolM2 elRol;
+        public Entidad elUsuario;
+        public Entidad elUsuario1;
+        public Entidad elUsuario2;
+        #endregion
+
+        #region SetUp and TearDown
+        [SetUp]
+        public void init()
+        {
+            elRol = new RolM2(1);
+            elUsuario = DominioTangerine.Fabrica.FabricaEntidades.crearUsuarioCompleto("Daniel", "1234", new DateTime(2015, 2, 10),"Activo", elRol,1);
+            elUsuario1 = DominioTangerine.Fabrica.FabricaEntidades.crearUsuarioCompleto("GianJose", "1234", new DateTime(2015, 2, 10), "Activo", elRol, 1);
+
+        }
+
+        [TearDown]
+        public void clean()
+        {
+            elUsuario = null;
+            elUsuario1 = null;
+            elUsuario2 = null;
+        }
+        #endregion
+
+        #region Test
+        /// <summary>
+        /// Prueba que permite verificar el insertar de un usuario en la base de datos.
+        /// </summary>
+        [Test]
+        public void TestAgregar()
+        {
+            IDAOUsuarios daoUsuario = DatosTangerine.Fabrica.FabricaDAOSqlServer.crearDaoUsuario();
+            //Inserto un Usuario
+            answer = daoUsuario.Agregar(elUsuario);
+
+            elUsuario1 = daoUsuario.ConsultarXId(DominioTangerine.Fabrica.FabricaEntidades.crearUsuarioCompletoConID(daoUsuario.ConsultLastUserID(),"GianJose", "1234", new DateTime(2015, 2, 10), "Activo", elRol, 1));
+            Assert.IsFalse(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).Id == daoUsuario.ConsultLastUserID());
+            Assert.IsFalse(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).nombreUsuario == "Daniel");
+            Assert.IsFalse(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).contrasena == "1234");
+            Assert.IsFalse(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).fechaCreacion == new DateTime(2015, 2, 10));
+            Assert.IsFalse(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).activo == "Activo");
+            Assert.IsFalse(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).rol == elRol);
+            Assert.IsFalse(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).fichaEmpleado == 1);
+
+        }
+
+        [Test]
+        public void TestModificar()
+        {
+            IDAOUsuarios daoUsuario = DatosTangerine.Fabrica.FabricaDAOSqlServer.crearDaoUsuario();
+            //Inserto un Usuario
+            answer = daoUsuario.Agregar(elUsuario);
+            /*
+            //elUsuario1 = daoUsuario.ConsultarXId(DominioTangerine.Fabrica.FabricaEntidades.crearCompaniaConId(daoCompania.ConsultLastCompanyId(),"CompaniaPrueba3", "J-111134112", "affdd@asdd.com", "34342344", "ADD", new DateTime(2015, 2, 10), 1, 100, 30, 1));
+            //Assert.IsTrue(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).Id == daoUsuario.consultLastUserId());
+            Assert.IsTrue(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).nombreUsuario == "Daniel");
+            Assert.IsTrue(((DominioTangerine.Entidades.M2.UsuarioM2)elUsuario1).contrasena == "1234");
+            */
+        }
+        #endregion
+
+        public Entidad dao { get; set; }
+    }
+}
