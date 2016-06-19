6e7a8d7542af10ac202c4928926a90d953358f5e
toniojua <antonio11346@gmail.com>
2016-06-18 20:25:31 -0430

    M3 Contrato consultar por id lead

---
+++ Tangerine/Tangerine/Tangerine_Contratos/M3/IContratoConsultarClientePotencial.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Web.UI.WebControls;
+
+namespace Tangerine_Contratos.M3
+{
+    public interface IContratoConsultarClientePotencial
+    {
+        string NombreEtiqueta { get; set; }
+        string EstatusEtiqueta { get; set; }
+        string RIFEtiquete { get; set; }
+        string PresupuestoInicial { get; set; }
+        string Correo { get; set; }
+        string NumLlamadas { get; set; }
+        string NumVisitas { get; set; }
+    }
+}
