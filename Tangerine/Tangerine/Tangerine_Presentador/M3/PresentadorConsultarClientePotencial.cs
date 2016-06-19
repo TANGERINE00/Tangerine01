b5ca2feb231a712aad045fb756b33e3403c6ec0b
toniojua <antonio11346@gmail.com>
2016-06-18 21:03:11 -0430

    M3 Consultar lead con presentador

--- Tangerine/Tangerine/Tangerine_Presentador/M3/PresentadorConsultarClientePotencial.cs
+++ Tangerine/Tangerine/Tangerine_Presentador/M3/PresentadorConsultarClientePotencial.cs
@@ -4,7 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using Tangerine_Contratos.M3;
-using LogicaTangerine;
+using LogicaTangerine.M3;
 using DominioTangerine;
 using System.Web;
 
@@ -17,6 +17,38 @@ namespace Tangerine_Presentador.M3
         public PresentadorConsultarClientePotencial(IContratoConsultarClientePotencial vista)
         {
             this.vista = vista;
+        }
+
+        public void Llenar(int idCliente)
+        {
+            LogicaM3 prueba = new LogicaM3();
+            int idClientePotencial = idCliente;
+            ClientePotencial elClientePotencial = prueba.BuscarClientePotencial(idClientePotencial);
+            try
+            {
+                vista.NombreEtiqueta.Text = elClientePotencial.NombreClientePotencial;
+                vista.RIFEtiqueta.Text = elClientePotencial.RifClientePotencial;
+                vista.CorreoEtiqueta.Text = elClientePotencial.EmailClientePotencial;
+                vista.PresupuestoInicialEtiqueta.Text = elClientePotencial.PresupuestoAnual_inversion.ToString();
+                vista.NumLlamadasEtiqueta.Text = elClientePotencial.NumeroLlamadas.ToString();
+                vista.NumVisitasEtiqueta.Text = elClientePotencial.NumeroVisitas.ToString();
+                if (elClientePotencial.Status == 0)
+                {
+                    vista.EstatusEtiqueta.Text = ResourceInterfaz.Inactivo + ResourceInterfaz.CloseSpanInact;
+                }
+                if (elClientePotencial.Status == 1)
+                {
+                    vista.EstatusEtiqueta.Text = ResourceInterfaz.Activo + ResourceInterfaz.CloseSpanAct;
+                }
+                if (elClientePotencial.Status == 2)
+                {
+                    vista.EstatusEtiqueta.Text = ResourceInterfaz.Promovido + ResourceInterfaz.CloseSpanProm;
+                }
+            }
+            catch (Exception ex)
+            {
+
+            }
         }
     }
 }
