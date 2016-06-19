a79eb0bcd3b8370dd009c2fdfc1f01ef8886e80d
Paul Yelamo <paulgyg91@gmail.com>
2016-06-18 23:26:47 -0400

    Merge conflictos resueltos

--- Tangerine/Tangerine/GUI/M7/modificarProyecto.aspx.designer.cs
+++ Tangerine/Tangerine/GUI/M7/modificarProyecto.aspx.designer.cs
@@ -85,15 +85,6 @@ namespace Tangerine.GUI.M7 {
         protected global::System.Web.UI.WebControls.TextBox textInputPorcentaje;
         
         /// <summary>
-        /// btnGuardar control.
-        /// </summary>
-        /// <remarks>
-        /// Auto-generated field.
-        /// To modify move field declaration from designer file to code-behind file.
-        /// </remarks>
-        protected global::System.Web.UI.WebControls.Button btnGuardar;
-        
-        /// <summary>
         /// inputGerente control.
         /// </summary>
         /// <remarks>
