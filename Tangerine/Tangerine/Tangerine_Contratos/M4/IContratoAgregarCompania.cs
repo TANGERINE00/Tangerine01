940fff1c268ae666fe551fc30206afd2a38feee6
omfigueira <omfr.13@gmail.com>
2016-06-20 19:17:38 -0400

    M4

--- Tangerine/Tangerine/Tangerine_Contratos/M4/IContratoAgregarCompania.cs
+++ Tangerine/Tangerine/Tangerine_Contratos/M4/IContratoAgregarCompania.cs
@@ -9,5 +9,14 @@ namespace Tangerine_Contratos.M4
 {
     public interface IContratoAgregarCompania
     {
+        string inputNombre1 { get; set; }
+        string inputAcronimo1 { get; set; }
+        string inputRIF1 { get; set; }
+        DropDownList inputDireccion1 { get; set; }
+        string inputEmail1 { get; set; }
+        string inputTelefono1 { get; set; }
+        string Datepicker1 { get; set; }
+        string inputPresupuesto1 { get; set; }
+        string inputPlazoPago1 { get; set; }
     }
 }
