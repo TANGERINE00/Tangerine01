1e8b56a13aa4b983bd08c9949c4c36774d5ebc96
edgkid <edglandaeta.15@gmail.com>
2016-06-19 21:32:53 -0400

    Consulta sobrre modificacion de leads (lista)

--- Tangerine/Tangerine/GUI/M3/ModificarLead.aspx.cs
+++ Tangerine/Tangerine/GUI/M3/ModificarLead.aspx.cs
@@ -99,24 +99,10 @@ namespace Tangerine.GUI.M3
         protected void Page_Load(object sender, EventArgs e)
         {
              int idClip = int.Parse(Request.QueryString["idclp"]);
-              /*if (!IsPostBack)
+              if (!IsPostBack)
               {
-                  LogicaM3 clientePotencialLogic = new LogicaM3();
-                  elClientePotencial = clientePotencialLogic.BuscarClientePotencial(idClip);
-                  //elClientePotencial = clientePotencialLogic.ModificarNuevoclientePotencial(idClip);
-
-                // elClientePotencial = clientePotencialLogic.ModificarNuevoclientePotencial(elClientePotencial);
-
-
-                  this.nombre.Value = elClientePotencial.NombreClientePotencial;
-                  this.rif.Value = elClientePotencial.RifClientePotencial;
-                  this.email.Value = elClientePotencial.EmailClientePotencial;
-
-                  this.presupuesto.Value = elClientePotencial.PresupuestoAnual_inversion.ToString();
-                  this.llamadas.Value = elClientePotencial.NumeroLlamadas.ToString();
-                  this.visitas.Value = elClientePotencial.NumeroVisitas.ToString();
-                      
-              }*/
+                  presentador.Llenar(idClip);        
+              }
           }
 
 
--- Tangerine/Tangerine/Tangerine_Presentador/M3/PresentadorModificarClientePotencial.cs
+++ Tangerine/Tangerine/Tangerine_Presentador/M3/PresentadorModificarClientePotencial.cs
@@ -19,6 +19,43 @@ namespace Tangerine_Presentador.M3
             this.vista = vista;
         }
 
+        public void Llenar(int idCliente)
+        {
+            Entidad _entidad = DominioTangerine.Fabrica.FabricaEntidades.ObtenerClientePotencial();
+            _entidad.Id = idCliente;
+
+            Comando<Entidad> comando = LogicaTangerine.Fabrica.FabricaComandos.ObtenerComandoConsultarClientePotencial(_entidad);
+            Entidad elClientePotencial = comando.Ejecutar();
+            try
+            {
+                DominioTangerine.Entidades.M3.ClientePotencial elCliente = (DominioTangerine.Entidades.M3.ClientePotencial)elClientePotencial;
+
+                vista.NombreEtiqueta = elCliente.NombreClientePotencial;
+                vista.RifEtiqueta = elCliente.RifClientePotencial;
+                vista.CorreoElectronico = elCliente.EmailClientePotencial;
+                vista.PresupuestoInversion = elCliente.PresupuestoAnual_inversion;
+                vista.NumeroLlamadas = elCliente.NumeroLlamadas;
+                vista.NumeroVisitas = elCliente.NumeroVisitas;
+
+                /*if (elCliente.Status == 0)
+                {
+                    vista..Text = ResourceInterfaz.Inactivo + ResourceInterfaz.CloseSpanInact;
+                }
+                if (elCliente.Status == 1)
+                {
+                    vista.EstatusEtiqueta.Text = ResourceInterfaz.Activo + ResourceInterfaz.CloseSpanAct;
+                }
+                if (elCliente.Status == 2)
+                {
+                    vista.EstatusEtiqueta.Text = ResourceInterfaz.Promovido + ResourceInterfaz.CloseSpanProm;
+                }*/
+            }
+            catch (Exception ex)
+            {
+                throw ex;
+            }
+        }
+
 
     }
 }
