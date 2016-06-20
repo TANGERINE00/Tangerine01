7ad3787f9fe302b4ee59204180ffe6005349f707
Luis Rodriguez <lrodriguezp.18@gmail.com>
2016-06-20 19:45:41 -0400

    log en comandos m5

--- Tangerine/Tangerine/LogicaTangerine/Comandos/M5/ComandoEliminarContactoDeProyecto.cs
+++ Tangerine/Tangerine/LogicaTangerine/Comandos/M5/ComandoEliminarContactoDeProyecto.cs
@@ -1,6 +1,7 @@
 ﻿using DatosTangerine.Fabrica;
 using DatosTangerine.InterfazDAO.M5;
 using DominioTangerine;
+using ExcepcionesTangerine;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -30,6 +31,10 @@ namespace LogicaTangerine.Comandos.M5
         /// <returns></returns>
         public override bool Ejecutar()
         {
+            Logger.EscribirInfo( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+                                 RecursoComandosM5.MensajeInicioInfoLogger,
+                                 System.Reflection.MethodBase.GetCurrentMethod().Name );
+
             bool respuesta = false;
 
             try 
@@ -39,8 +44,13 @@ namespace LogicaTangerine.Comandos.M5
             }
             catch ( Exception ex )
             {
+                Logger.EscribirError( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex );
                 return respuesta;
             }
+
+            Logger.EscribirInfo( System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name,
+                                 RecursoComandosM5.MensajeFinInfoLogger,
+                                 System.Reflection.MethodBase.GetCurrentMethod().Name );
 
             return respuesta;
         }
