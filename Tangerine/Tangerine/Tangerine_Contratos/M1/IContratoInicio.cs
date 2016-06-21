671a9e4e6cbd092b81f7e89639ca5cf6699f2d7e
Eduardo Pacheco <arzagoth@gmail.com>
2016-06-21 00:35:11 -0400

    M1_Cambios
    cambios

--- Tangerine/Tangerine/Tangerine_Contratos/M1/IContratoInicio.cs
+++ Tangerine/Tangerine/Tangerine_Contratos/M1/IContratoInicio.cs
@@ -8,26 +8,9 @@ namespace Tangerine_Contratos.M1
 {
     public interface IContratoInicio
     {
-        String UserNameEtq { get; set; }
+        string userInput { get; set; }
 
-        String PasswordEtq { get; set; }
+        string passwordInput { get; set; }
 
-        Boolean ErrorLogin { get; set; }
-
-        Boolean WarningLog { get; set; }
-
-        Boolean InfoLog { get; set; }
-
-        Boolean SuccessLog { get; set; }
-
-        String ErrorLoginText { get; set; }
-
-        String WarningLogText { get; set; }
-
-        String InfoLogText { get; set; }
-
-        String SuccessLogText { get; set; }
-
-        String RestablecerCorreoEtq { get; set; }
     }
 }
