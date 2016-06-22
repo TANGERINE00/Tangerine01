ee71d8efad26c1d15b9ad1705b2e866cf5f60ca2
ToteDieguez <totedieguez@gmail.com>
2016-06-22 14:26:02 -0400

    Mejora de Consultar Registrar Usuario  M2

--- Tangerine/Tangerine/Tangerine_Presentador/M2/PresentadorRegistroUsuario.cs
+++ Tangerine/Tangerine/Tangerine_Presentador/M2/PresentadorRegistroUsuario.cs
@@ -55,48 +55,5 @@ namespace Tangerine_Presentador.M2
                 _vista.tablaEmpleado += ResourceGUIM2.CloseTR;
             }
         }
-
-        /// <summary>
-        /// Método para actualizar la vista a traves del boton buscar
-        /// </summary>
-        public void actualizarVista()
-        {
-            if ( _vista.numeroEmpleado.Equals( "" ) )
-            {
-                inicioVista();
-            }
-            else
-            {
-                _vista.tablaEmpleado = null;
-                DominioTangerine.Entidades.M10.EmpleadoM10 numeroEmpleado 
-                    = new DominioTangerine.Entidades.M10.EmpleadoM10( int.Parse( _vista.numeroEmpleado ) );
-                LogicaTangerine.Comando<Entidad> theComando = 
-                    LogicaTangerine.Fabrica.FabricaComandos.ConsultarIdEmpleado( numeroEmpleado );
-                Entidad theEmpleado = theComando.Ejecutar();
-                DominioTangerine.Entidades.M10.EmpleadoM10 empleado = ( DominioTangerine.Entidades.M10.EmpleadoM10 )theEmpleado;
-                LogicaTangerine.Comando<Boolean> theComandoVerificar
-                    = LogicaTangerine.Fabrica.FabricaComandos.verificarUsuario( empleado.emp_id );
-                _vista.tablaEmpleado += ResourceGUIM2.OpenTR;
-                _vista.tablaEmpleado += ResourceGUIM2.OpenTD + empleado.emp_id.ToString() + ResourceGUIM2.CloseTD;
-                _vista.tablaEmpleado += ResourceGUIM2.OpenTD + empleado.Emp_p_nombre + ResourceGUIM2.CloseTD;
-                _vista.tablaEmpleado += ResourceGUIM2.OpenTD + empleado.Emp_p_apellido + ResourceGUIM2.CloseTD;
-                _vista.tablaEmpleado += ResourceGUIM2.OpenTD + empleado.Emp_cedula + ResourceGUIM2.CloseTD;
-                _vista.tablaEmpleado += ResourceGUIM2.OpenTD + empleado.jobs.Nombre + ResourceGUIM2.CloseTD;
-                if (!theComandoVerificar.Ejecutar())
-                {
-                    _vista.tablaEmpleado += ResourceGUIM2.OpenTD + ResourceGUIM2.BotonRegNuevaVentana + empleado.emp_id +
-                                            ResourceGUIM2.NombreEmpleado + empleado.emp_p_nombre + ResourceGUIM2.ApellidoEmpleado +
-                                            empleado.emp_p_apellido + ResourceGUIM2.CloseBotonParametro + ResourceGUIM2.CloseTD;
-                }
-                else
-                {
-                    _vista.tablaEmpleado += ResourceGUIM2.OpenTD + ResourceGUIM2.BotonRegBlock +
-                                            ResourceGUIM2.CloseBotonParametroDesactivado + ResourceGUIM2.CloseTD;
-                }
-
-                _vista.tablaEmpleado += ResourceGUIM2.CloseTR;
-                _vista.numeroEmpleado = null;
-            }
-        }
     }
 }
