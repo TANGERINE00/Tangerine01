d403b75f73fa29a4dfbdcbae55c725ddc698d101
Gianfranco Berlino <giantufano@gmail.com>
2016-06-22 16:08:31 -0400

    Drop Procedure

--- Tangerine/Tangerine/Script de borrado - DB Tangerine.sql
+++ Tangerine/Tangerine/Script de borrado - DB Tangerine.sql
@@ -42,6 +42,7 @@ drop procedure M3_consultar_cliente_potencial;
 drop procedure M3_modificar_clientePotencialF;
 drop procedure M3_activar_cliente_potencial;
 drop procedure M3_eliminar_cliente_potencial_def;
+drop procedure M3_ConsultarIdUltimoLead
 
 drop procedure M4_AgregarCompania;
 drop procedure M4_ConsultarCompania;
