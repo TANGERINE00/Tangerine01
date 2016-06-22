5f779f66132a26dfbb812b3ef5bbdccb45794842
alanlenner <alanmucho@hotmail.com>
2016-06-21 20:18:13 -0400

    Adelanto modificar propuesta

--- Tangerine/Tangerine/Tangerine_Contratos/M6/IContratoModificarPropuesta.cs
+++ Tangerine/Tangerine/Tangerine_Contratos/M6/IContratoModificarPropuesta.cs
@@ -3,10 +3,68 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 namespace Tangerine_Contratos.M6
 {
     public interface IContratoModificarPropuesta
     {
+        string ContenedorCompania
+        {
+            get;
+            set;
+        }
+        string IdCompania
+        {
+            get;
+        }
+        string Descripcion
+        {
+            get;
+        }
+        DropDownList ComboDuracion
+        {
+            get;
+            set;
+        }
+        string TextoDuracion
+        {
+            get;
+        }
+        string DatePickerUno
+        {
+            get;
+        }
+        string DatePickerDos
+        {
+            get;
+        }
+        DropDownList TipoCosto
+        {
+            get;
+            set;
+        }
+
+        string TextoCosto
+        {
+            get;
+        }
+        DropDownList FormaPago
+        {
+            get;
+            set;
+        }
+        DropDownList ComboCuota
+        {
+            get;
+            set;
+        }
+        DropDownList ComboStatus
+        {
+            get;
+            set;
+        }
+
+
     }
 }
