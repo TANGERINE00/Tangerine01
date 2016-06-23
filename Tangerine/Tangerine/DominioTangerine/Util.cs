c4e7d01c13fd344fa8a2cfd49dfdecd9521c99d2
ToteDieguez <totedieguez@gmail.com>
2016-06-23 15:47:51 -0400

    Eliminación clases primera entrega M2

--- Tangerine/Tangerine/DominioTangerine/Util.cs
+++ Tangerine/Tangerine/DominioTangerine/Util.cs
@@ -1,4 +1,4 @@
-﻿using System;
+﻿/*using System;
 using System.Collections.Generic;
 using System.Linq;
 using System.Text;
@@ -12,4 +12,4 @@ namespace DominioTangerine
 
         public static bool MASTER_FLAG = true;
     }
-}
+}*/
