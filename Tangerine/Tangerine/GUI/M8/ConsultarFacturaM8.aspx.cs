1de8f1e3e7f2b568ec865f1b6c7d8ffc3c67db29
jmri1508 <jrirausquin.1995@gmail.com>
2016-06-23 09:02:33 -0430

    Eliminacion de la carpeta vieja m4 de logica y datos

--- Tangerine/Tangerine/GUI/M8/ConsultarFacturaM8.aspx.cs
+++ Tangerine/Tangerine/GUI/M8/ConsultarFacturaM8.aspx.cs
@@ -6,7 +6,6 @@ using System.Web.UI;
 using System.Web.UI.WebControls;
 using DominioTangerine;
 using LogicaTangerine;
-using LogicaTangerine.M4;
 using LogicaTangerine.M7;
 using Tangerine_Presentador.M8;
 using Tangerine_Contratos.M8;
