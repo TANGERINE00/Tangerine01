b628386806e406780807821fe5be8013e964b394
Eduardo Pacheco <arzagoth@gmail.com>
2016-06-25 19:43:38 -0400

    ValidacionBD_M1

--- Tangerine/Tangerine/Tangerine_Presentador/Master/PresentadorMasterPage.cs
+++ Tangerine/Tangerine/Tangerine_Presentador/Master/PresentadorMasterPage.cs
@@ -29,60 +29,75 @@ namespace Tangerine_Presentador.Master
         /// </summary>
         public void CargarSesion()
         {
-            if (HttpContext.Current.Session["User"] == null)
-                HttpContext.Current.Response.Redirect("../M1/Login.aspx");
-            else
+            try
             {
-                if (( UtilM1.MASTER_FLAG) && (HttpContext.Current.Session["Rol"] != null))
+
+
+                if (HttpContext.Current.Session["User"] == null)
+                    HttpContext.Current.Response.Redirect("../M1/Login.aspx");
+                else
                 {
-
-                    Uri thisPageUrl = HttpContext.Current.Request.Url;
-                    string pathDePaginaActal = thisPageUrl.AbsolutePath;
-                    string nombreRol = HttpContext.Current.Session["Rol"].ToString();
-
-                    //Creación y Ejecución del Objeto Comando de verificacion de acceso a la pagina, se le envia 
-                    //por parámetro el path de la pagina y el rol del usuario
-                   
-                    Comando<bool> comando =
-                     LogicaTangerine.Fabrica.FabricaComandos.obtenerComandoVerificarAccesoAPagina(pathDePaginaActal, nombreRol);
-
-                    bool privilegioAcceso = comando.Ejecutar();
-
-                    if (privilegioAcceso)
+                    if ((UtilM1.MASTER_FLAG) && (HttpContext.Current.Session["Rol"] != null))
                     {
-                        HttpContext.Current.Response.Redirect("../M1/Dashboard.aspx");
-                    }
-                    else
-                    {
-                        Comando<List<string>> comandoL = 
-                            LogicaTangerine.Fabrica.FabricaComandos.obtenerComandoVerificarAccesoAOpciones(nombreRol);
-                        List<string> bloqueos = comandoL.Ejecutar();
-                       
 
-                        foreach (string s in bloqueos)
+                        Uri thisPageUrl = HttpContext.Current.Request.Url;
+                        string pathDePaginaActal = thisPageUrl.AbsolutePath;
+                        string nombreRol = HttpContext.Current.Session["Rol"].ToString();
+
+                        //Creación y Ejecución del Objeto Comando de verificacion de acceso a la pagina, se le envia 
+                        //por parámetro el path de la pagina y el rol del usuario
+
+                        Comando<bool> comando =
+                         LogicaTangerine.Fabrica.FabricaComandos.obtenerComandoVerificarAccesoAPagina(pathDePaginaActal, nombreRol);
+
+                        bool privilegioAcceso = comando.Ejecutar();
+
+                        if (privilegioAcceso)
                         {
-                            System.Diagnostics.Debug.WriteLine(s);
-                            _iMaster.IFindControl(s);
-                            
+                            HttpContext.Current.Response.Redirect("../M1/Dashboard.aspx");
+                        }
+                        else
+                        {
+                            Comando<List<string>> comandoL =
+                                LogicaTangerine.Fabrica.FabricaComandos.obtenerComandoVerificarAccesoAOpciones(nombreRol);
+                            List<string> bloqueos = comandoL.Ejecutar();
+
+
+                            foreach (string s in bloqueos)
+                            {
+                                System.Diagnostics.Debug.WriteLine(s);
+                                _iMaster.IFindControl(s);
+
+                            }
                         }
                     }
                 }
+
+
+                if (HttpContext.Current.Session["User"] != null)
+                    _iMaster.sesionUsuario = HttpContext.Current.Session["User"] + "";
+
+
+                if (HttpContext.Current.Session["User"] != null)
+                {
+                    _iMaster.sesionUsuario = HttpContext.Current.Session["User"] + "";
+                    _iMaster.usuarioDet = HttpContext.Current.Session["Rol"] + "";
+                    _iMaster.fechaUser = HttpContext.Current.Session["Date"] + "";
+                }
+
+                else
+                    _iMaster.sesionUsuario = "Usuario";
             }
-
-
-            if (HttpContext.Current.Session["User"] != null)
-               _iMaster.sesionUsuario = HttpContext.Current.Session["User"] + "";
-            
-
-            if (HttpContext.Current.Session["User"] != null)
+            catch (ExcepcionesTangerine.M10.BaseDatosException e)
             {
-                _iMaster.sesionUsuario = HttpContext.Current.Session["User"] + "";
-                _iMaster.usuarioDet = HttpContext.Current.Session["Rol"] + "";
-                _iMaster.fechaUser = HttpContext.Current.Session["Date"] + "";
+                HttpContext.Current.Session.Abandon();
+                HttpContext.Current.Response.Redirect("../M1/Login.aspx");
             }
-
-            else
-                _iMaster.sesionUsuario = "Usuario";
+            catch (Exception e)
+            {
+                HttpContext.Current.Session.Abandon();
+                HttpContext.Current.Response.Redirect("../M1/Login.aspx");
+            }
         }
 
         /// <summary>
