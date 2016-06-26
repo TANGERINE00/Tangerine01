ccf51681636d950a2985c1649956cbc875e00952
leojma <leojma@gmail.com>
2016-06-26 18:33:46 -0400

    M10 arreglo

--- Tangerine/Tangerine/PruebasUnitarias/M4/PruebaLogica.cs
+++ Tangerine/Tangerine/PruebasUnitarias/M4/PruebaLogica.cs
@@ -56,6 +56,7 @@ namespace PruebasUnitarias.M4
             DatosTangerine.InterfazDAO.M4.IDaoCompania daoCompania = DatosTangerine.Fabrica.FabricaDAOSqlServer.crearDaoCompania();
             Comando<bool> Comand = FabricaComandos.CrearAgregarCompania(theCompany);
             Assert.IsTrue(Comand.Ejecutar());
+            Comando<Entidad> Comand2 = FabricaComandos.CrearConsultarCompania(DominioTangerine.Fabrica.FabricaEntidades.CrearCompaniaConId(daoCompania.ConsultLastCompanyId(),"CompaniaPrueba3", "J-111111113", "asd@asdddd.com", "3434234", "ASS", new DateTime(2015, 2, 10), 1, 100, 30, 1));
             theCompany2 = Comand2.Ejecutar();
             Assert.IsTrue(((DominioTangerine.Entidades.M4.CompaniaM4)theCompany2).Id == daoCompania.ConsultLastCompanyId());
             Assert.IsTrue(((DominioTangerine.Entidades.M4.CompaniaM4)theCompany2).NombreCompania == "CompaniaPrueba3");
