528301dbd3f6aab5b23356067e2ef0954bc1155d
ieba <istvanbokor8@gmail.com>
2016-06-26 18:09:12 -0400

    ajustes correo casi final

--- Tangerine/Tangerine/GUI/M8/EnviarCorreoM8.aspx.cs
+++ Tangerine/Tangerine/GUI/M8/EnviarCorreoM8.aspx.cs
@@ -50,7 +50,8 @@ namespace Tangerine.GUI.M8
 
         public string adjunto
         {
-            get { return this.adjuntar.PostedFile.FileName; }
+            get { return this.Label1.Text; }
+            set { this.Label1.Text = value; }
         }
         public string alertaClase
         {
@@ -66,6 +67,9 @@ namespace Tangerine.GUI.M8
         {
             set { alert.InnerHtml = value; }
         }
+
+
+
         #endregion
 
         protected void Page_Load(object sender, EventArgs e)
@@ -86,9 +90,70 @@ namespace Tangerine.GUI.M8
 
         protected void buttonEnviarCorreo_Click(object sender, EventArgs e)
         {
+            if (fuImage.HasFile)
+            {
+                SaveFile(fuImage.PostedFile);
+                // SaveAs method of PostedFile property used
+                // to save the file at specified rooted path
+                //fuImage.PostedFile.SaveAs(Server.MapPath("~/Facturas") + System.IO.Path.DirectorySeparatorChar + fuImage.PostedFile.FileName);
+                
+                // success message
+                //adjunto = Server.MapPath("~/Facturas") + System.IO.Path.DirectorySeparatorChar + fuImage.PostedFile.FileName;
+            }
 
-            if (_presentador.enviarCorreo())
+            if (_presentador.enviarCorreo(adjunto))
                 Server.Transfer(ResourceGUIM8.volver);
+        }
+
+        void SaveFile(HttpPostedFile file)
+        {
+            // Specify the path to save the uploaded file to.
+            string savePath = Server.MapPath("~/Facturas") + System.IO.Path.DirectorySeparatorChar;
+
+            // Get the name of the file to upload.
+            string fileName = file.FileName;
+
+            // Create the path and file name to check for duplicates.
+            string pathToCheck = savePath + fileName;
+
+            // Create a temporary file name to use for checking duplicates.
+            string tempfileName = "";
+
+            // Check to see if a file already exists with the
+            // same name as the file to upload.        
+            if (System.IO.File.Exists(pathToCheck))
+            {
+                int counter = 2;
+                while (System.IO.File.Exists(pathToCheck))
+                {
+                    // if a file with this name already exists,
+                    // prefix the filename with a number.
+                    tempfileName = counter.ToString() + fileName;
+                    pathToCheck = savePath + tempfileName;
+                    counter++;
+                }
+
+                fileName = tempfileName;
+
+                // Notify the user that the file name was changed.
+                //UploadStatusLabel.Text = "A file with the same name already exists." + "<br />Your file was saved as " + fileName;
+                int i = 1;
+            }
+            else
+            {
+                // Notify the user that the file was saved successfully.
+                //UploadStatusLabel.Text = "Your file was uploaded successfully.";
+                int i = 1;
+            }
+
+            // Append the name of the file to upload to the path.
+            savePath += fileName;
+
+            // Call the SaveAs method to save the uploaded
+            // file to the specified directory.
+            file.SaveAs(savePath);
+            adjunto = savePath;
+
         }
     }
 }
