be46115d8a6ce129c7133f06d5161361338fcfc9
Katherine Lindo <kathelindo@hotmail.com>
2016-06-27 19:57:28 -0400

    M9 Se agregan comentarios

--- Tangerine/Tangerine/ExcepcionesTangerine/M9/WrongFormatExceptionM9Tangerine.cs
+++ Tangerine/Tangerine/ExcepcionesTangerine/M9/WrongFormatExceptionM9Tangerine.cs
@@ -8,7 +8,12 @@ namespace ExcepcionesTangerine.M9
 {
     public class WrongFormatExceptionM9Tangerine:ExceptionsTangerine
     {
-
+        /// <summary>
+        /// Excepción que se arroja cuando se pase un parametro en un formato incorrecto
+        /// </summary>
+        /// <param name="codigo"></param>
+        /// <param name="message"></param>
+        /// <param name="inner"></param>
         public WrongFormatExceptionM9Tangerine(string codigo, string message, Exception inner)
             : base(codigo, message, inner)
         {
