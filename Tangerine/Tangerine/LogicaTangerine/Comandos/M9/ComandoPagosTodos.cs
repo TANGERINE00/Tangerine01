787bbc83263d9172b7ea75952bc4754e7f8e227f
Katherine Lindo <kathelindo@hotmail.com>
2016-06-27 19:18:04 -0400

    M9 Se modifica PU de comando agregar pago

--- Tangerine/Tangerine/LogicaTangerine/Comandos/M9/ComandoPagosTodos.cs
+++ Tangerine/Tangerine/LogicaTangerine/Comandos/M9/ComandoPagosTodos.cs
@@ -13,10 +13,44 @@ namespace LogicaTangerine.Comandos.M9
     /// <summary>
     /// Comando para consultar todos los pagos
     /// </summary>
-    class ComandoPagosTodos 
+    public class ComandoPagosTodos : Comando<List<Entidad>>
     {
 
+        /// <summary>
+        /// Metodo que ejecuta el comando
+        /// </summary>
+        /// <returns>booleano que refleja el exito de la ejecucion del comando</returns>
+        public override List<Entidad> Ejecutar()
+        {
+            try
+            {
+                Logger.EscribirInfo(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name
+                , ResourceComandoM9.MensajeInicioInfoLogger, System.Reflection.MethodBase.GetCurrentMethod().Name);
 
+                IDAOPago daoPago = FabricaDAOSqlServer.CrearDAOPago();
+                List<Entidad> respuesta = daoPago.ConsultarTodos();
+                return respuesta;
+            }
+            catch (ArgumentNullException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExcepcionesTangerine.M4.NullArgumentException(ResourceComandoM9.CodigoErrorNull,
+                    ResourceComandoM9.MensajeErrorNull, ex);
+
+            }
+            catch (FormatException ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+
+                throw new ExcepcionesTangerine.M8.WrongFormatException(ResourceComandoM9.CodigoErrorFormato,
+                     ResourceComandoM9.MensajeErrorFormato, ex);
+            }
+            catch (Exception ex)
+            {
+                Logger.EscribirError(System.Reflection.MethodBase.GetCurrentMethod().DeclaringType.Name, ex);
+                throw new ExcepcionesTangerine.ExceptionsTangerine(ResourceComandoM9.MensajeGenerico, ex);
+            }
+        }
 
 
     }
