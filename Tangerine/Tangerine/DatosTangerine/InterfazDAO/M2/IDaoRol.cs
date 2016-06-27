3c9d067f33b68b73d032e88ab1a96e9bc70e38e1
ToteDieguez <totedieguez@gmail.com>
2016-06-27 16:55:57 -0400

    Mejoras en documentación en IDAORol

--- Tangerine/Tangerine/DatosTangerine/InterfazDAO/M2/IDaoRol.cs
+++ Tangerine/Tangerine/DatosTangerine/InterfazDAO/M2/IDaoRol.cs
@@ -13,29 +13,29 @@ namespace DatosTangerine.InterfazDAO.M2
         /// <summary>
         /// Metodo para modificar el rol del usuario
         /// </summary>
-        /// <param name="usuario"></param>
+        /// <param name="usuario">Atributo de tipo Entidad, que posee los datos de un usuario</param>
         /// <returns>Retorna true cuando el rol del usuario se modifico exitosamente</returns>
         bool ModificarRolUsuario( Entidad usuario );
 
         /// <summary>
         /// Metodo que retorna el rol de un usuario
         /// </summary>
-        /// <param name="codigoRol"></param>
+        /// <param name="codigoRol">El codigo de un rol en especifico</param>
         /// <returns>El rol del usuario</returns>
         Entidad ObtenerRolUsuario( int codigoRol );
 
         /// <summary>
         /// Metodo que obtiene la opciones de un menu
         /// </summary>
-        /// <param name="nombreMenu"></param>
-        /// <param name="codigoRol"></param>
+        /// <param name="nombreMenu">El menu que sera proporpecionado en el sistema</param>
+        /// <param name="codigoRol">El codigo de un rol en especifico</param>
         /// <returns>Una lista con las opciones</returns>
         Entidad ObtenerOpciones( string nombreMenu , int codigoRol );
 
         /// <summary>
         /// Metodo que devuelve el rol del usuario
         /// </summary>
-        /// <param name="nombreRol"></param>
+        /// <param name="nombreRol">El nombre de un rol, por ejemplo, gerente</param>
         /// <returns>Devuelve el rol del usuario por nombre</returns>
         Entidad ObtenerRolUsuarioPorNombre( string nombreRol );
 
